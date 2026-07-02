c328cd01b9b239ac30df9efc79a3103c7c3cab01
HumanConsumer <113416857+HumanConsumer@users.noreply.github.com>
2026-07-02 09:36:10 +0200

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,3 @@
-
 import network
 import time
 import socket
@@ -9,12 +8,67 @@ from neopixel import Neopixel
 from utime import sleep
 from farve import FARVE, REGNBUE_SEKVENS
 
-ssid = "wifinavn"
-word = "wifipassword"
+ssid = "navn"
+word = "password!!!"
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, word)
+
+trekantNum = [[0,42],[43,62],[63,82],[83,102],[103,122],]
+
+pixels=Neopixel(trekantNum[-1][-1]+1,0,0,"GRB")
+
+# Funktion til at lave en gradient i hver trekant
+# trekant: 0-4 - valg af trekant
+# farve1: hvilken farve der skal startes fra. Se farve.py for mulige farver
+# farve2: hvilken farve der skal gradieres til
+# lysstyrke: 0-255
+def sæt_gradient_trekant(trekant, farve1, farve2, lysstyrke):
+    start_pixel = trekantNum[trekant][0]
+    slut_pixel = trekantNum[trekant][-1]
+    pixels.set_pixel_line_gradient(start_pixel, slut_pixel, farve1, farve2, lysstyrke)
+    pixels.show()
+# Funktion der sætter farven i en hel trekant.
+# trekant: 0-4 - valg af trekant
+# farve: se filen farve.py for de farver der kan tilgås
+# lysstyrke: 0-255
+def sæt_farve_trekant(trekant, farve, lysstyrke):
+    start_pixel = trekantNum[trekant][0]
+    slut_pixel = trekantNum[trekant][-1]
+    pixels.set_pixel_line(start_pixel, slut_pixel, farve, lysstyrke)
+    pixels.show()
+
+
+# Funktion til at lave en gradient i over hele lampen
+# farve1: hvilken farve der skal startes fra. Se farve.py for mulige farver
+# farve2: hvilken farve der skal gradieres til
+# lysstyrke: 0-255
+def sæt_gradient_lampe(farve1, farve2, lysstyrke):
+    start_pixel = trekantNum[0][0]
+    slut_pixel = trekantNum[-1][-1]
+    pixels.set_pixel_line_gradient(start_pixel, slut_pixel, farve1, farve2, lysstyrke)
+    pixels.show()
+    
+# Funktion der sætter farven i hele lampen.
+# farve: se filen farve.py for de farver der kan tilgås
+# lysstyrke: 0-255
+def sæt_farve_lampe(farve, lysstyrke):
+    pixels.brightness(lysstyrke)
+    pixels.fill(farve)
+    pixels.show()
+    
+# Funktion der laver en gradient i de fire foreste trekanter
+# farve1: hvilken farve der skal startes fra. Se farve.py for mulige farver
+# farve2: hvilken farve der skal gradieres til
+# lysstyrke: 0-255
+def sæt_gradient_forside(farve1, farve2, lysstyrke):
+    start_pixel = trekantNum[1][0]
+    slut_pixel = trekantNum[-1][-1]
+    pixels.set_pixel_line_gradient(start_pixel, slut_pixel, farve1, farve2, lysstyrke)
+    pixels.show()
+
+
 if not wlan.isconnected():
     sæt_farve_lampe(FARVE['lilla'],255)
     time.sleep(1)
